b214e8a052309c983bb2303fa8538adfbf5cee5c
OBADA ALHAWJREH <obada7jaber7@gmail.com>
2021-04-25 17:27:34 +0300

    lab06herko

---
+++ server.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const express = require('express');
+require('dotenv').config();
+
+const cors = require('cors');
+
+const server = express();
+
+const PORT = process.env.PORT || 4000;
+
+server.use(cors()); 
+
+server.get('/data',(req,res)=>{
+  res.status(200).send('Hi from the data page, I am the server !!!');
+});
+
+server.get('/location',(req,res)=>{
+  let locationData = require('./data/location.json');
+
+  let locationRes = new Location(locationData);
+  console.log(locationRes);
+
+  res.send(locationRes);
+
+});
+
+function Location(locData){
+  this.search_query = 'Lynnwood';
+  this.formatted_query = locData[0].display_name;
+  this.latitude = locData[0].lat;
+  this.longitude = locData[0].lon;
+}
+
+server.get('*',(req,res)=>{
+  let errObj = {
+    status: 404,
+    resText: 'sorry! wrong page :)'
+  };
+  res.status(404).send(errObj);
+});
+
+server.listen(PORT,()=>{
+  console.log(`listening on port ${PORT}`);
+});
